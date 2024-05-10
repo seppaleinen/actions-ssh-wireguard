bea0878989f9e77ce5de26e6ba4bc269d8c4652e
daveri <david.eriksson@avanza.se>
2024-05-10 17:20:37 +0200

    Try to not use echo, to get around github actions redacting the data

--- runner.sh
+++ runner.sh
@@ -17,8 +17,8 @@ sudo apt-get install -y wireguard openssh-client resolvconf
 
 echo "Configuring WireGuard..."
 # Create wireguard config
-echo "$WIREGUARD_CONFIG" | sudo tee /etc/wireguard/wg0.conf
-echo "$SSH_KEY" | sudo tee /ssh.pub
+sudo tee /etc/wireguard/wg0.conf <<< "$WIREGUARD_CONFIG"
+sudo tee /ssh.pub <<< "$SSH_KEY"
 
 echo "Starting WireGuard..."
 # Start wireguard
